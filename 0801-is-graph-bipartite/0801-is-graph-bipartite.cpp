bc514a1ca20337bd106e08e61f254164b5348d7d
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-02-26 22:15:22 +0530

    Time: 0 ms (100%), Space: 17.5 MB (27.49%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,36 +1,67 @@
+
+// class Solution {
+// public:
+
+//     bool checkBipartiteBFS(vector<int>adj[], int curr, vector<int>& color, int currColor) {
+       
+//         color[curr] = currColor; //color kardiya curr node ko
+        
+//         queue<int> que;
+//         que.push(curr);
+        
+//         while(!que.empty()) {
+//             int u = que.front();
+//             que.pop();
+//             // //ab ek ek krke q se element nikal kr unke phuchne wale nikalenege
+//             for(int &v : adj[u]) {//check traverse krne wale ka color same toh ni h
+//                 if(color[v] == color[u]) {
+//                     return false;
+//                 } else if(color[v] == -1) {
+//                     color[v] = 1 - color[u];
+//                     que.push(v);// //bad m kya pta iske adj node ko b toh traverse krna pdega na kyoki isme color hee nhi kiya gya thaa atoh ab kuya gya h toh mtlb iske adj node traverse krne baki 
+//                 }
+//             }
+//         }
+        
+//         return true;
+//     }
+
+
 class Solution {
 public:
-  bool CheckBipertite(vector<vector<int>>& graph , int curr , vector<int>&color , int currcolor ){
-    color[curr] = currcolor ;  //phle to color krdo then condition check krdo
-     
-     for( int &v : graph[curr]){
-        if( color[curr]==color[v]){ //first condition ki curr color or color[v] traverse krne wala color same h mtlb adj node same color k hoge joh kbhi ni ho skta
-            return false;
-        }
-         if( color[v] == -1){
-          int  colorofV = 1- currcolor;
-           //mtlb color khali h isliye color pta kro kon sa bhra jayega uske liye trick h
+    bool checkBipartite(std::vector<std::vector<int>>& graph, int curr, std::vector<int>& color, int currColor) {
+        color[curr] = currColor;
+        std::queue<int> q;
+        q.push(curr);
 
-         
-        if( CheckBipertite(graph , v , color , colorofV)==false)
-            return false;
-        } 
-     
-     }
-        return true;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
 
-  }
-    bool isBipartite(vector<vector<int>>& graph) {
-         int V = graph.size();
-         vector<int>color(V , -1);
-
-         for( int i =0 ; i<V ; i++){
-            if(color[i]==-1){
-                if(CheckBipertite(graph , i , color , 1)== false)
-                return false;
+            for (int &v : graph[u]) {
+                if (color[v] == color[u]) {
+                    return false;  // If adjacent nodes have the same color, it's not bipartite
+                } 
+                else if (color[v] == -1) {
+                    color[v] = 1 - color[u];  // Assign opposite color
+                    q.push(v);  // Push `v`, not `u`
+                }
             }
-         }
+        }
         return true;
-         
+    }
+
+    bool isBipartite(std::vector<std::vector<int>>& graph) {
+        int V = graph.size();
+        std::vector<int> color(V, -1);
+
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                if (!checkBipartite(graph, i, color, 1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
