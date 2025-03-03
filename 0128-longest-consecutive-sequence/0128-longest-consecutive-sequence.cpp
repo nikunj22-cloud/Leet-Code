a70cc4b06a3dc2532eb594a3ea159da1d4bf8e95
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-03-03 21:44:00 +0530

    Time: 76 ms (69.23%), Space: 87.9 MB (31.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<int> st(nums.begin(), nums.end()); // Insert all elements into the set
         int ans = 0;
 
-        for (int num : nums) {
+        for (int num : st) { // Iterate through the set, not the vector
             // Check if the current number is the start of a sequence
             if (st.find(num - 1) == st.end()) {
                 int curr = num;
