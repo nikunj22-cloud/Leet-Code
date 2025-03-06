46296050358d716ead0e2e3f3ee5b96c444e0609
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-03-06 13:29:59 +0530

    Time: 0 ms (100%), Space: 15.1 MB (25.09%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -7,21 +7,21 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
+ */  //constraints no 3 says ki n ki value kbhi b jyada ni hogi size se
 class Solution {
 public:
-   int getLengthOfLinkedList(ListNode* head){
-    int length = 0;
-    while(head!= NULL){
-        head = head->next;
-        length++;
-    }
-    return length;
+     int getLength( ListNode* head ){
+        int length = 0;
+        while( head!= NULL){
+            head = head->next;
+            length++;
+        }
+        return length;
 
-   }
+    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int L = getLengthOfLinkedList(head);
-        if( n== L){
+        int L = getLength(head);
+        if(n== L){
             ListNode* temp = head->next;
             delete(head);
             return temp;
@@ -30,11 +30,11 @@ public:
         ListNode* prev = NULL;
         ListNode* temp = head;
         while(traverse--){
-              prev = temp;
-              temp = temp->next;         
+            prev = temp ;
+            temp = temp->next ;
         }
         prev->next = temp->next;
         delete(temp);
-        return head;
+        return head;  
     }
 };
