2036438d05c8c7d77c02b65d00caee080d833756
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-03-07 12:06:32 +0530

    Time: 0 ms (100%), Space: 19.5 MB (62.26%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -13,19 +13,15 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1 == NULL)return list2;
         if(list2 == NULL)return list1;
-        ListNode* result;
-
-        if( list1->val > list2->val){
+          ListNode* result;
+        if(list1->val > list2->val){
             result = list2;
-
             result->next = mergeTwoLists( list1 , list2->next);
         }
         else{
             result = list1;
-            result->next = mergeTwoLists( list1->next , list2);
-
+            result->next = mergeTwoLists(list1->next , list2);
         }
         return result;
-        
     }
 };
