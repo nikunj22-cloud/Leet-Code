d957eedc29a35b2a7d20dbd141dd31c75e51a2cf
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-03-07 17:04:06 +0530

    Time: 0 ms (100%), Space: 22.7 MB (50.64%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,22 @@
 class Solution {
-public: 
-    int removeDuplicates(vector<int>& nums) {
-       int n = nums.size(); 
-       int i =0 ;
-       int j  = 1 ;
-      while( j< n){
-      
-        if( nums[i] != nums[j]){
-            i++; //phle i ko aage bdhayenge kyoki i jha pr hoga wha b unique elemenet store hoga isliye agr whi pr dusra 
+public:
+  //phle i ko aage bdhayenge kyoki i jha pr hoga wha b unique elemenet store hoga isliye agr whi pr dusra 
             //unique element store kr diya toh glt hoga
-            nums[i] =  nums[j];
-            
+            //agr num[i]!= num[j] agr esa h hee nhi toh j ko aage bdhate rhenge jb tk unique element na mil jaye
+            //i+1 element return krenge kyoki i zero base indexing pr h toh total agr ans m 5 element hoge toh 4 return krega isliye
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        int j = 1;
+        int n = nums.size();
+
+        while(j< n){
+            if(nums[i]!= nums[j]){
+                i++;
+            nums[i] = nums[j];
+            }
+            j++;
         }
-        j++; //agr num[i]!= num[j] agr esa h hee nhi toh j ko aage bdhate rhenge jb tk unique element na mil jaye
-       }
-       return i+1;
+        
+        return i+1;
     }
 };
