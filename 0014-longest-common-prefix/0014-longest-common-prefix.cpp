48e110731852367722252271adbe51987fe375a1
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-03-20 08:49:58 +0530

    Time: 0 ms (100%), Space: 12.1 MB (21.71%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        //sort the string
         sort(strs.begin() , strs.end());
-        
-        //compare first and last of the string
-        string first = strs[0]; //first string
-        string last = strs[strs.size() -1]; //second string
-        string result = ""; //result joh string store krega
-        for( int i = 0 ; i< first.size() ; i++){ //loop chlaya or compare krra first string ko last string se
-            if(first[i] != last[i]) break; //agr element same nhi h toh break kr diya
-            result += first[i]; //nhi toh result m add krdiya koi si bkrlo prefix  hee add hogi
-        }
-        return result;
+       string first = strs[0];
+        string last = strs[strs.size()-1];
+       string result = "";//output khali b aa skta h without space remember
+    
+     for( int i = 0 ; i<first.size() ; i++){
+         if(first[i] != last[i])break;
+         result += first[i];
+     }   
+     return result;
     }
 };
