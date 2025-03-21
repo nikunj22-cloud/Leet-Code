6159f39634ce7fd299e74e0c35e0474564af39fa
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-03-21 18:47:35 +0530

    Time: 0 ms (100%), Space: 10.4 MB (99.33%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,15 +11,42 @@
  */
 class Solution {
 public:
-      void inorder( TreeNode* root , vector<int>&result){
-        if(!root)return;
-        inorder(root->left , result);
-        result.push_back(root->val);
-        inorder(root->right , result);
-      }
+    //   void inorder( TreeNode* root , vector<int>&result){
+    //     if(!root)return;
+    //     inorder(root->left , result);
+    //     result.push_back(root->val);
+    //     inorder(root->right , result);
+    //   }
+    //using morris inorder
     vector<int> inorderTraversal(TreeNode* root) {
-             vector<int>result;
-             inorder(root , result);
-             return result;
+            //  vector<int>result;
+            //  inorder(root , result);
+            //  return result;
+            TreeNode*curr  = root;
+            vector<int>result;
+            while(curr != NULL){
+                if( curr->left == NULL){
+                    result.push_back(curr->val);
+                    curr = curr->right;
+                }
+                else{
+                    TreeNode*leftChild = curr->left;
+                    while(leftChild->right != NULL){
+                        leftChild = leftChild->right;
+                    }
+                    leftChild->right = curr;
+
+                    //ab curr ko aage bdhakrr delete krne ki bari h
+                    TreeNode* temp = curr ;
+                    curr = curr->left;//yr yeh yad rkhna pdega kyoki yeh dry run krke smz nhi aaya
+                    temp->left = NULL;
+
+                }
+
+                 
+
+            }
+            return result;
     }
-};
+}; //tc o(n)wcase
+//sc o(n) wcase
