cbcb7be4042f5dd1f1df57ab04c1f8aeafc4ae8e
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-04-03 07:55:18 +0530

    Time: 85 ms (52.14%), Space: 88.9 MB (32.87%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,24 +1,26 @@
 class Solution {
-public:
+public: //yeh pura code yad rkhna pdega kyoki yeh time limit ka error de rha h so yad rko
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> st(nums.begin(), nums.end()); // Step 1: Insert all elements in set
+        unordered_set<int> st(nums.begin(), nums.end()); // Insert all elements into the set
         int ans = 0;
-        
-        for (int i = 0; i < nums.size(); i++) { // Step 2: Use normal for loop
-            if (st.find(nums[i] - 1) == st.end()) { // Check if nums[i] is the start of a sequence
-                int seq = nums[i];
+
+        for (int num : st) { // Iterate through the set, not the vector
+            // Check if the current number is the start of a sequence
+            if (st.find(num - 1) == st.end()) { //mtlb set main yeh phle present nhi thee  toh simple num ko phle sequnce mankr aage bdho
+                int curr = num;
                 int count = 1;
 
-                while (st.find(seq + 1) != st.end()) { // Continue sequence
-                    seq++;
+                // Count the length of the sequence //map main value mil gyi h or woh variable ko aagebdha do
+                while (st.find(curr + 1) != st.end()) {
+                    curr++;
                     count++;
                 }
 
-                ans = max(ans, count); // Update max sequence length
+                // Update the maximum length
+                ans = max(ans, count);
             }
         }
 
         return ans;
     }
 };
-;
