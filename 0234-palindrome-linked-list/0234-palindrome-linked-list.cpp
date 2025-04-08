7242f7bac55bb079f1696aa2ae9d8ccd419db13e
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-04-08 09:02:56 +0530

    Time: 4 ms (53.92%), Space: 124.9 MB (37.76%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,21 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+  //beacuse empty list b palindrom bnati h
+  //recursion chlega head ->next jb tk head ka next null na ho
+  //curr joh ki head pr h aage bdhta rhega recursion ka kuch ni h woh toh traverse krlega apne aap backtracking k through but curr ko compare krne k liye backtrack krna pdega
 class Solution {
 public:
-ListNode* curr;
-bool recur(ListNode* head){
-    if(head==NULL)return true; //beacuse empty list b palindrom bnati h
-    bool ans = recur(head->next);
-        if(head->val != curr->val){
+    ListNode* curr;
+    
+    bool checkPalindrom(ListNode* head) {
+        if(head == NULL)
+            return true;
+            
+        bool check = checkPalindrom(head->next);
+        if(head->val != curr->val) {  //false se hee check krenge kyoki curr head k last m phuch gya agr last wali value same nhi nikli toh aage bdn ka koi fayda nhi h isk=liye yhi se false return krdenge
             return false;
         }
-    curr = curr->next;
-    return ans;
-}
-    bool isPalindrome(ListNode* head) {
-     curr = head;
-      return recur(head);   
+        curr = curr->next;
+        return check;  
     }
-    
+    bool isPalindrome(ListNode* head) {
+        curr = head;
+        return checkPalindrom(head);
+    }
 };
