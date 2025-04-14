7f79b49cf2ff0701222228933f84ef933612c81f
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-04-14 18:15:57 +0530

    Time: 1 ms (54.95%), Space: 13.9 MB (83.26%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,24 +1,31 @@
 class Solution {
-    public: 
-    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds) {
-        if(target==0) {
-            ans.push_back(ds);
-            return;
-        }        
-        for(int i = ind;i<arr.size();i++) {
-            if(i>ind && arr[i]==arr[i-1]) continue; 
-            if(arr[i]>target) break; 
-            ds.push_back(arr[i]);
-            findCombination(i+1, target - arr[i], arr, ans, ds); 
-            ds.pop_back(); 
-        }
-    }
 public:
+     void solve( vector<int>&candidates , vector<vector<int>>&result , int target , int idx , vector<int>&comb){
+        if( target == 0){
+            result.push_back(comb);
+            return;
+        }
+        if( idx == candidates.size()){
+            return;
+        }
+          if( target < 0){
+            return;
+          }
+        for( int i = idx ; i< candidates.size() ; i++){
+            if(  i > idx && candidates[i] == candidates[i-1]){
+              continue;
+            }
+            comb.push_back(candidates[i]);
+            solve( candidates , result , target-candidates[i] , i+1, comb);
+            comb.pop_back();
+        }
+     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector<vector<int>> ans; 
-        vector<int> ds; 
-        findCombination(0, target, candidates, ans, ds); 
-        return ans; 
+         sort( candidates.begin() , candidates.end());
+         vector<int>comb;
+         vector<vector<int>>result;
+         solve( candidates , result , target  , 0 , comb);
+
+         return result;
     }
 };
