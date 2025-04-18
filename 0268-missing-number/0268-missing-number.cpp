64d5899fcddbdc3fe2d00d023bcc96474c398d38
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-04-18 08:53:40 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.69%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = n*(n+1)/2;
+        int sum1 = 0;
+        for( int i = 0 ; i < nums.size() ; i++){
+            sum1 += nums[i];
+        }
+        int find = sum - sum1;
+
+        return find;
+    }
+};
