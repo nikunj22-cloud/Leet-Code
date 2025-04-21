66a0887f64e80b2fd3f88b34aacc4f77682d0d8d
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-04-21 18:24:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,57 +1,37 @@
 class Solution {
 public:
-    // Function to find the pivot (smallest element) in the rotated sorted array
-    int pivot_element(vector<int>& nums, int n) {
-        int l = 0, r = n - 1;
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] > nums[r]) {
-                l = mid + 1; // Pivot is in the right half
-            } else {
-                r = mid; // Pivot is in the left half
+    int search(vector<int>& arr, int target) {
+         int  i = 0;
+         int j = arr.size()-1;
+
+         while( i<=j){
+            int mid = i + (j-i)/2;
+            if( arr[mid]==target){
+                return  mid ;
             }
-        } 
-        return l; // Return the index of the smallest element becasuse it is design to find lowest element
-    }
-
-    // Standard binary search function
-    int binarySearch(int l, int r, int target, vector<int>& nums) {
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target) {
-                return mid; // Target found
-            } else if (nums[mid] > target) {
-                r = mid - 1; // Search in the left half
-            } else {
-                l = mid + 1; // Search in the right half
+            //left sorted toh left pr binary search lgao
+            if( arr[i] <= arr[mid]){
+            //check krlo left m mera target element h ya nhi agr nhi toh move right of left 
+            if( arr[i] <= target && target <= arr[mid]){
+                j = mid-1;
             }
-        }
-        return -1; // Target not found
-    }
+            else{
+                i = mid+1;
+            }
 
-    // Main search function
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        if (n == 0) return -1; // Edge case: empty array
+            }
+            else{
+             //right sorted h toh right pr binary seach lgao
+             if(mid <= target && target <= arr[j]){
+                //check krlo right m mera target element h ya nhi agr nhi toh move left of right
+               i = mid+1;
+             }
+             else{
+                j = mid-1;
+             }
 
-        // Find the pivot index
-        int pivot = pivot_element(nums, n);
-
-        // If the target is the pivot element
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-
-        // Decide which part of the array to search
-        if (pivot == 0) {
-            // Array is not rotated, search the entire array
-            return binarySearch(0, n - 1, target, nums);
-        } else if (target >= nums[0] && target <= nums[pivot - 1]) {
-            // Target is in the left sorted part
-            return binarySearch(0, pivot - 1, target, nums);
-        } else {
-            // Target is in the right sorted part
-            return binarySearch(pivot, n - 1, target, nums);
-        }
+            }
+         }
+         return -1;
     }
 };
