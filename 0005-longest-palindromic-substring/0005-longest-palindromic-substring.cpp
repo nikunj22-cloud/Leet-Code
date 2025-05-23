10d83eb5e419eb5cc3696a6063a94c2a627be222
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-05-23 10:00:57 +0530

    Time: 184 ms (26.67%), Space: 13.8 MB (56.7%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    int t[1001][1001];
-    
-    bool solve(string &s, int l, int r){
-        if(l >= r) 
+ int  t[1001][1001];
+     bool palindrom( string& s , int i ,int j){
+        if( i >= j){
             return 1;
-
-        if(t[l][r] != -1){
-            return t[l][r];
         }
-
-        if(s[l] == s[r]) {
-            return t[l][r] = solve(s, l+1, r-1);
+          if(t[i][j] != -1){
+            return t[i][j];
+          }
+        if( s[j]== s[i]){
+            return  t[i][j] = palindrom( s , i+1 , j-1);
         }
-
-        return t[l][r] = false;
-    }
-    
+        return t[i][j] = 0;
+     }
     string longestPalindrome(string s) {
-        int n = s.length();
-        
-        int maxlen = INT_MIN;
-        int startingIndex = 0;
-
-        memset(t, -1, sizeof(t));
-
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                
-                if(solve(s, i, j)) {
-                    if(j-i+1 > maxlen){
-                        startingIndex = i;
-                        maxlen = j-i+1;
-                    }
+           int n = s.length();
+          memset( t , -1 , sizeof(t));
+        int maxsize = INT_MIN;
+        int stpoint = 0;
+       
+       
+        for( int i = 0 ; i<n ; i++){
+           for( int j = i ; j< n ; j++){
+             if(palindrom(s , i , j)==true){
+                if( j-i+1 > maxsize){
+                    maxsize = j-i+1;
+                stpoint =  i;
                 }
-                
-            }
+             }
+           }
         }
-
-        return s.substr(startingIndex, maxlen);
+        return s.substr( stpoint , maxsize);
     }
 };
