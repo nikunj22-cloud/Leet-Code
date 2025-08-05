f66cf62bdbd31d800706127225119ba0b375066d
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-08-05 16:00:20 +0530

    Time: 0 ms (100%), Space: 10.1 MB (80.27%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+         int t[101];
+         int solve(vector<int>&nums , int n   , int i){
+            if( i>= n){
+                return 0;
+            }
+             if( t [i] != -1){
+                return t[i];
+             }
+            int steal  = nums[i] + solve(nums , n , i+2 );
+            int skip = solve(nums , n , i+1);
+
+            return t[i]  = max(steal , skip);
+         }
+    int rob(vector<int>& nums) {
+         int n = nums.size() ;
+         memset( t , -1 ,  sizeof(t));
+        return  solve(nums , n , 0);
+    }
+};
