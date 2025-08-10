57ee77b10d4907cdea3831e7750ea9cbc44cd92a
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-08-10 11:56:51 +0530

    Time: 0 ms (100%), Space: 17.6 MB (37.99%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,67 +1,34 @@
-
-// class Solution {
-// public:
-
-//     bool checkBipartiteBFS(vector<int>adj[], int curr, vector<int>& color, int currColor) {
-       
-//         color[curr] = currColor; //color kardiya curr node ko
-        
-//         queue<int> que;
-//         que.push(curr);
-        
-//         while(!que.empty()) {
-//             int u = que.front();
-//             que.pop();
-//             // //ab ek ek krke q se element nikal kr unke phuchne wale nikalenege
-//             for(int &v : adj[u]) {//check traverse krne wale ka color same toh ni h
-//                 if(color[v] == color[u]) {
-//                     return false;
-//                 } else if(color[v] == -1) {
-//                     color[v] = 1 - color[u];
-//                     que.push(v);// //bad m kya pta iske adj node ko b toh traverse krna pdega na kyoki isme color hee nhi kiya gya thaa atoh ab kuya gya h toh mtlb iske adj node traverse krne baki 
-//                 }
-//             }
-//         }
-        
-//         return true;
-//     }
-
-
 class Solution {
-public:
-    bool checkBipartite(std::vector<std::vector<int>>& graph, int curr, std::vector<int>& color, int currColor) {
-        color[curr] = currColor;
-        std::queue<int> q;
-        q.push(curr);
-
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-
-            for (int &v : graph[u]) {
-                if (color[v] == color[u]) {
-                    return false;  // If adjacent nodes have the same color, it's not bipartite
-                } 
-                else if (color[v] == -1) {
-                    color[v] = 1 - color[u];  // Assign opposite color
-                    q.push(v);  // Push `v`, not `u`
+public: 
+     bool bfs( vector<vector<int>>& graph , int currcolor , int node , vector<int>& color){
+        queue<int>que;
+        que.push(node);
+        color[node] = currcolor;
+        while( !que.empty()){
+            int u = que.front();
+            que.pop();
+            for( int v : graph[u]){
+                if( color[v] == color[u]){
+                    return false;
+                }
+                if( color[v] == -1){
+                    color[v] = 1- color[u];
+                    que.push(v);
                 }
             }
         }
         return true;
-    }
-
-    bool isBipartite(std::vector<std::vector<int>>& graph) {
-        int V = graph.size();
-        std::vector<int> color(V, -1);
-
-        for (int i = 0; i < V; i++) {
-            if (color[i] == -1) {
-                if (!checkBipartite(graph, i, color, 1)) {
+     }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int v = graph.size();
+         vector<int>color( v , -1);
+         for( int i = 0 ; i< v ; i++){
+            if(color[i] == -1){
+               if (!bfs(graph, 0, i, color)) {
                     return false;
                 }
             }
-        }
-        return true;
+         }
+         return true;
     }
 };
