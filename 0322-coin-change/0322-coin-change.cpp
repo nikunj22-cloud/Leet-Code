8c836e18b3f58edab72a14d81dfd73259f24dd19
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-08-12 12:33:13 +0530

    Time: 31 ms (63.01%), Space: 13.7 MB (97.05%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+     int t[10001];
+
+    int solve(vector<int>& coins, int amount) {
+        if (amount < 0) return INT_MAX; // invalid
+        if (amount == 0) return 0;      // base case
+
+        if (t[amount] != -1) return t[amount]; // already computed
+
+        int mini = INT_MAX;
+        for (int i = 0; i < coins.size(); i++) {
+            int ans = solve(coins, amount - coins[i]);
+            if (ans != INT_MAX) {
+                mini = min(mini, ans + 1);
+            }
+        }
+
+        return t[amount] = mini;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        memset( t , -1 , sizeof(t)); // initialize memo table
+
+        int ans = solve(coins, amount);
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
