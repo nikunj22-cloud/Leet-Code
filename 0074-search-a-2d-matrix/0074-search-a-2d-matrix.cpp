cc8b2d73796a158f0b060003fa2cf80849e23333
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-08-15 19:27:10 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty() || matrix[0].empty()) return false;
+        
         int rows = matrix.size();
-        int cols = matrix[0].size();
-        int r = 0;
-        int c = cols-1;
-        if(matrix.empty() || matrix[0].empty()){
-            return false;
-        } //traverse from top right corner because agr target right se bda hua toh mtlb agli row m hoga asending order m h or agr chota hua toh left m hoga
-        while(r< rows && c >= 0 ){
-              if(matrix[r][c]==target){
+        int columns = matrix[0].size();
+        
+        int left = 0;
+        int right = rows * columns - 1;
+        
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / columns][mid % columns]; // Convert 1D index back to 2D indices
+            
+            if(midValue == target) {
                 return true;
-              }
-              else if(matrix[r][c]  > target){
-                c--;
-              }
-              else{
-                r++;
-              }
-              
+            } else if(midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-      return false;  
+        
+        return false;
     }
 };
-// It is the pivot point where we can decide to move left or down:
-// Left: Elements get smaller.
-// Down: Elements get larger.
