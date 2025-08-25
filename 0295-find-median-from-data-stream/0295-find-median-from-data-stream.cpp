6c5a08b3ef7cf9ba44f51ad18c3701c7380e91fb
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-08-25 22:00:47 +0530

    Time: 73 ms (39%), Space: 148.7 MB (9.13%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,43 +1,38 @@
 class MedianFinder {
 public:
-   priority_queue<int>left_max_heap;
-   priority_queue<int , vector<int> , greater<int>>right_min_heap;
+    priority_queue<int>leftmaxheap;
+    priority_queue<int , vector<int> , greater<int>>rightminheap;
     MedianFinder() {
-          //khali chor
+        
     }
     
     void addNum(int num) {
-        //do condition check krlo 
-        //theek h number dalne ko bol rha h  dalde normal no ko check krke daldo
-        //agr aane wala no chota h left max heap k top element se to direct dal do left max heap m
-        if(left_max_heap.empty() || num < left_max_heap.top()){
-            left_max_heap.push(num);
+        if( leftmaxheap.empty() || num < leftmaxheap.top() ){
+            leftmaxheap.push(num);
         }
         else{
-            right_min_heap.push(num);
+            rightminheap.push(num);
         }
-        //theek h number toh dal diya ab yeh dekhna h ki no ko shi jgh toh dala h mtlb left max heap or right min heap k size m diff ya toh 0  ho ya fir 1 ho 
-
-        if(abs((int)left_max_heap.size() - (int)right_min_heap.size()) > 1){
-            right_min_heap.push(left_max_heap.top());
-            left_max_heap.pop();
+        if( abs((int) leftmaxheap.size() - (int) rightminheap.size()) > 1 ){
+             rightminheap.push(leftmaxheap.top());
+             leftmaxheap.pop();
         }
-        else if(left_max_heap.size() < right_min_heap.size()){
-                left_max_heap.push(right_min_heap.top());
-                right_min_heap.pop();
-        }
-
+         else if ( leftmaxheap.size() < rightminheap.size()){
+               leftmaxheap.push(rightminheap.top());
+               rightminheap.pop();
+         }
+        
     }
     
     double findMedian() {
-        double median;
-        if(left_max_heap.size() == right_min_heap.size()){
-           return ((double)left_max_heap.top() + right_min_heap.top())/2;
-        }
-        else{
-            return left_max_heap.top();
-        }
-        return median;
+        
+      if( leftmaxheap.size()== rightminheap.size()){
+             
+             
+             return (double)(leftmaxheap.top() + rightminheap.top())/2;;
+         }
+        
+           return   leftmaxheap.top();
     }
 };
 
