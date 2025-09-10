c3c15094c0be481b895b1036e991042edceb06c3
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-10 22:41:19 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty() || matrix[0].empty()) return false;
+         if(matrix.empty() || matrix[0].empty()) return false;
         
-        int rows = matrix.size();
-        int columns = matrix[0].size();
-        
+        int n = matrix.size();
+        int row = matrix.size();
+        int col = matrix[0].size();
         int left = 0;
-        int right = rows * columns - 1;
-        
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            int midValue = matrix[mid / columns][mid % columns]; // Convert 1D index back to 2D indices
-            
-            if(midValue == target) {
+        int right = row*col-1;
+        while( left <= right){
+            int mid = left + (right-left)/2;
+            if( matrix[mid/col][mid%col] == target){ //1 d arr k mid 2d arr k mid seint n = matrix.size();
                 return true;
-            } else if(midValue < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            }
+            else if( matrix[mid/col][mid%col] > target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
             }
         }
-        
         return false;
     }
 };
