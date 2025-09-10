87f18411aaa8b89f1964342ab3a8c8eee95dadf2
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-10 18:16:59 +0530

    Time: 3 ms (89.44%), Space: 23.8 MB (57.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> merge(vector<vector<int>>& inter) {
         vector<vector<int>>ans;
-        int n = intervals.size();
-        //step 1 sort
-        sort(intervals.begin() , intervals.end());
-        for( int i = 0 ; i< n ; i++){
-            if( ans.empty() || intervals[i][0] > ans.back()[1]){ //new interval found
-                ans.push_back(intervals[i]);
-            } 
-            else{
-                   ans.back()[1] = max( ans.back()[1] , intervals[i][1]);
+        sort(inter.begin() , inter.end());
+        for( int i = 0 ; i<inter.size() ; i++){
+            if( ans.empty() || inter[i][0] > ans.back()[1]){
+                ans.push_back( inter[i]);
             }
-
+            else{
+                ans.back()[1] = max( inter[i][1] , ans.back()[1]);
+            }
         }
-        return ans ;
-
+        return ans;
     }
 };
+//Iteration 1: i = 0 → inter[0] = [1,3]
+
+// ans is empty → push [1,3]
+
+// ans = [[1,3]]
+
+// Iteration 2: i = 1 → inter[1] = [2,6]
+
+// Check: inter[1][0] = 2 > ans.back()[1] = 3? ❌ (2 ≤ 3) → overlapping
+
+// Merge: ans.back()[1] = max(6, 3) = 6
