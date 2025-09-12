bdf63d75b554154c716715389a8a85d6c9f1b92e
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-12 20:12:41 +0530

    Time: 9 ms (96%), Space: 17.4 MB (55.95%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,36 +1,35 @@
 class Solution {
-public: 
-vector<vector<int>>ans;
+public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>result;
+        sort( nums.begin() , nums.end());
          int n = nums.size();
-         sort( nums.begin() , nums.end());
-         for(  int first = 0 ; first< n-3 ; first++){
-            if( first > 0 && nums[first]== nums[first-1])continue; //because of unique quadrent
-            for(int second = first+1 ; second< n-2 ; second++){
-                if( second > first+1  && nums[second] == nums[second-1])continue;
-                 int third = second+1;
-                 int forth = n-1;
-                 while( third< forth){ //sum is defined in to the loop
-                     long long sum = (long long)nums[first] + (long long)nums[second] + (long long)nums[third] + (long long )nums[forth];
-                    if( sum > target){
-                        forth--;
+        for( int i = 0 ; i<nums.size() ;i++){
+            if( i > 0 && nums[i] == nums[i-1]) continue;
+            for( int j = i+1 ; j < nums.size() ; j++){
+                if( j > i+1 && nums[j] == nums[j-1])continue;
+                int l = j+1 ; 
+                int k = n-1;
+                while( l < k){
+                     long long  sum = (long long)nums[i]+ nums[j]+nums[l] + nums[k];
+                    if( sum == target){
+                        result.push_back({nums[i] , nums[j] ,nums[l] , nums[k]});
+                    
+                    k--;
+                    l++;
+                    while( l < k && nums[l] == nums[l-1]) l++;//while loop h isliye ++ hoga agr if ki condition hoti toh continue aata duplicate k liye 
+                    while( l < k && nums[k] == nums[k+1]) k--;
                     }
-                    else if ( sum < target){
-                        third++;
+                    else if( sum < target){
+                        l++;
                     }
                     else{
-                        //target mil gya 
-                        ans.push_back({nums[first] , nums[second] , nums[third] , nums[forth]});
-                        third++;
-                        forth--;
-                        
-                         while( third< forth && nums[third]== nums[third-1])third++; //remember in the else 
+                        k--;
                     }
-                   
-                    
-                 }
+                }
+                
             }
-         }
-         return ans;
+        }
+        return result;
     }
 };
