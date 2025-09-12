6fa719071c876f5d94b677ee1d901f1c517f790c
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-12 21:45:39 +0530

    Time: 22 ms (60.21%), Space: 17.4 MB (83.44%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>result;
-        sort( nums.begin() , nums.end());
-         int n = nums.size();
-        for( int i = 0 ; i<nums.size() ;i++){
-            if( i > 0 && nums[i] == nums[i-1]) continue;
-            for( int j = i+1 ; j < nums.size() ; j++){
-                if( j > i+1 && nums[j] == nums[j-1])continue;
-                int l = j+1 ; 
-                int k = n-1;
-                while( l < k){
-                     long long  sum = (long long)nums[i]+ nums[j]+nums[l] + nums[k];
-                    if( sum == target){
-                        result.push_back({nums[i] , nums[j] ,nums[l] , nums[k]});
-                    
-                    k--;
-                    l++;
-                    while( l < k && nums[l] == nums[l-1]) l++;//while loop h isliye ++ hoga agr if ki condition hoti toh continue aata duplicate k liye 
-                    while( l < k && nums[k] == nums[k+1]) k--;
-                    }
-                    else if( sum < target){
-                        l++;
-                    }
-                    else{
-                        k--;
-                    }
+      vector<vector<int>>result;
+      sort(nums.begin() , nums.end());
+      int n = nums.size();
+      for( int i = 0 ; i< n ; i++){
+
+        for( int j = i+1 ; j<n; j++){
+                    if( i > 0 && nums[i] == nums[i-1])continue;
+            if( j >i+1 && nums[j] == nums[j-1])continue;
+            int k = j+1;
+            int l = n-1;
+           
+            while( k < l){
+                 long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+                 //remember sum loop k ander define kiya jayega
+                if( sum == target ){
+                    result.push_back({nums[i] , nums[j] , nums[k] , nums[l]});
+                    k++;
+                    l--;
+                    while( k < l && nums[k] == nums[k-1] )k++;
+                    while( k < l && nums[l] == nums[l+1])l--;
                 }
-                
+                else if( sum < target){
+                    k++;
+                }
+                else {
+                    l--;
+                }
             }
-        }
-        return result;
+        } 
+      }
+      return result;   
     }
 };
