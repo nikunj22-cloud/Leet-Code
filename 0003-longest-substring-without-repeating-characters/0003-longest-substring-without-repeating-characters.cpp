ced3693732e31f7fee8a1bad311849e85d1975f9
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-14 00:03:10 +0530

    Time: 11 ms (50.34%), Space: 14.2 MB (36%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,10 +8,26 @@ public:
             while( st.find(s[right]) != st.end()){
                 st.erase( s[left]);
                 left++;
-                maxlen = max( maxlen , right-left+1);
+               
             }
             st.insert(s[right]);
+             maxlen = max( maxlen , right-left+1);
         } 
         return maxlen;
     }
 };
+//bhai hmko full window nikalni h jb tk duplicate ni mile tb tk isliye current window ka size hr bar insert krte hue dekhna pdega na ki kevl duplicate pr dekhna pde duplicate pr dekhnga toh agr current size ni nikal payega kyoki left toh aage bdh jayega 
+//Kyun update maxlength har iteration me
+
+// while loop sirf window ko shrink karta hai jab duplicate mile.
+
+// Duplicate milne ya na milne se current substring har step pe valid ho jaata hai.
+
+// Toh abhi ke right aur left ke beech ka window = current non-repeating substring.
+
+// maxlength = max(maxlength, right-left+1);
+
+
+// Yeh line har iteration me current window ka length check kar raha hai.
+
+// Agar yeh sirf duplicate ke time hota → tu duplicate na aane wale windows ko miss kar dega (jaise pure increasing substring "abc" me).
