df983acf61f2a88a1b0a1bd788a167d28a8d2611
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-15 18:00:06 +0530

    Time: 33 ms (80.61%), Space: 54.5 MB (73.01%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,36 +1,29 @@
 class Solution {
-public:
-  bool palindrom( string s){
-         int i = 0;
-         int j = s.length()-1;
-         while( i< j){
-            if(s[i] != s[j]){
-                return false;
-            }
-            i++;
-            j--;
-         }
-         return true;
-      }
-    void getallparts( vector<vector<string>>&ans , string s , vector<string>&part){
-         if(s.size() == 0){
-            ans.push_back(part);
-            return ;
-         }
-        for( int i = 0 ; i<s.length() ; i++){
-              string parti = s.substr( 0 , i+1);
-              if(palindrom(parti)){
-                part.push_back(parti);
-                getallparts(ans , s.substr(i+1) , part);
-                part.pop_back();
-              }
-        }
-    }
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
-        vector<string>part;
-         getallparts(ans , s , part);
-         return ans;
+public: 
+       bool ispalindrom( string s){
+        string s2 = s;
+        reverse(s2.begin() , s2.end());
+         return s == s2;
+       }
+     void getallparts( string s , vector<string>&partition , vector<vector<string>>&ans){
         
+          if( s.length() == 0 ){
+            ans.push_back(partition);
+            return ;
+          }
+          for( int i = 0 ; i< s.length() ; i++){
+             string part = s.substr( 0 , i+1);
+             if(  ispalindrom(part)){
+                 partition.push_back(part);
+                getallparts( s.substr(i+1) , partition , ans );
+                partition.pop_back();
+             }
+          }
+     }
+    vector<vector<string>> partition(string s) {
+         vector<string>partition;
+         vector<vector<string>>ans;
+         getallparts( s , partition , ans);
+         return ans;
     }
 };
