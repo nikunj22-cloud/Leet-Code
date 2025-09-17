d243ed3d523079e37d0e1f629a8b812461016cff
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-17 22:22:52 +0530

    Time: 3 ms (7.59%), Space: 9.9 MB (6.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;  //ham ulte elemenet insert krnege
-         for( char &ch : s){
+        stack<char>st;
+        for( auto ch : s){
             if( ch == '('){
                 st.push(')');
             }
-            else if(ch == '{'){
+            else if( ch == '{'){
                 st.push('}');
             }
             else if( ch == '['){
                 st.push(']');
             }
-            else if(st.empty() || st.top() != ch){ //yeh conditon tb bnegi jb stack ya toh empty h ya fir stack ka top element ch k barbar nhi h mtlb stack main koi closing bracket ni h
-                return false;
+            else if( st.empty() || st.top() != ch){
+                 return false;
             }
             else{
                 st.pop();
-            } //agr match ho gya element toh pop krdo 
-
-         }
-         return st.empty();
+            }
+        }
+        return st.empty();
     }
 };
