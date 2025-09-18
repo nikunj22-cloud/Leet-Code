d75ac4f88cea1c27a91e50d38c64e8a052f4f33b
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-18 20:05:20 +0530

    Time: 16 ms (83.49%), Space: 85.6 MB (47.45%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,52 +1,48 @@
 class Solution {
-public: 
-vector<int>nextSmaller( vector<int>& heights , int n){
-    stack<int>s;
-    vector<int>Ans(n);
-    s.push(-1);
-    for( int i = n-1 ; i>=0 ; i--){
-        int curr = heights[i];
-        while( s.top() != -1   &&  heights[s.top()]>= curr){
-            s.pop();
-        }
-        Ans[i] = s.top();
-        s.push(i);
-    }
-    return Ans;
-}
- vector<int>prevsmaller(vector<int>& heights , int n){
-    stack<int>s;
-    vector<int>Ans(n);
-    s.push(-1);
-    for( int i = 0 ; i<n ; i++){
-        int curr = heights[i];
-        while( s.top() != -1 && heights[s.top()] >= curr){
-            s.pop();
-        }
-        Ans[i] = s.top();
-        s.push(i);
-    }
-    return Ans;
- }
-   int largestRectangleArea(vector<int>& heights) {
-       int n = heights.size();
-         vector<int>next(n);
-        next = nextSmaller(heights , n);
-        vector<int>prev(n);
-         prev = prevsmaller(heights , n);
-
-       int Area = INT_MIN;
-       for( int i = 0 ; i<n ; i++){
-            int l = heights[i];
-
-            if( next[i]== -1){
-                next[i] = n;
+public:
+    vector<int> nextsmaller(vector<int>& heights, int n) {
+        stack<int> st;
+        vector<int> ans(n, -1);
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
             }
+            if (!st.empty()) {
+                ans[i] = st.top(); // index of next smaller
+            }
+            st.push(i); // push index
+        }
+        return ans;
+    }
 
-            int b = next[i] - prev[i] -1 ;
-             int newArea = l*b;
-             Area = max(newArea , Area);
-       }
-      return Area; 
+    vector<int> prevsmaller(vector<int>& heights, int n) {
+        stack<int> st;
+        vector<int> ans(n, -1);
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                ans[i] = st.top(); // index of prev smaller
+            }
+            st.push(i); // push index
+        }
+        return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> next = nextsmaller(heights, n);
+        vector<int> prev = prevsmaller(heights, n);
+
+        int maxarea = 0;
+        for (int i = 0; i < n; i++) {
+            int l = heights[i];
+            if (next[i] == -1) next[i] = n; // if no smaller on right
+            int b = next[i] - prev[i] - 1;
+            int area = l * b;
+            maxarea = max(area, maxarea);
+        }
+        return maxarea;
     }
 };
