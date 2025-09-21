cb049b5fc663075f4629be0e654150ff81461e91
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-21 16:10:48 +0530

    Time: 0 ms (100%), Space: 12.1 MB (17.88%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin() , strs.end());
-       string first = strs[0];
-       int n = strs.size();
-        string last = strs[n-1];
-       string result = "";//output khali b aa skta h without space remember
-    
-     for( int i = 0 ; i<first.size() ; i++){
-         if(first[i] != last[i])break;
+          sort( strs.begin() , strs.end());
+          string result = "";
+          int n = strs.size();
+          string first = strs[0];
+           string last = strs[n-1];
+         for( int i = 0 ; i< first.size() ; i++){ //remember first.size tk chlega bss or toh halua h
+                 if(first[i] != last[i])break;
          result += first[i];
-     }   
-     return result;
+         }
+         return result;
     }
 };
