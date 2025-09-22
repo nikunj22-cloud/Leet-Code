3a641e42b83109e1453963bfa40c189667be64f2
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-22 23:31:08 +0530

    Time: 4 ms (53.51%), Space: 10 MB (46.25%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     string countAndSay(int n) {
-       if( n==1) return "1";  //question main diya hua h
-
-       string say = countAndSay( n-1); //question m diya hua h
-       string result = "";
-
-       for( int i = 0 ; i < say.length() ; i++){
-          char ch = say[i];
-          int count = 1;
-
-          while( i< say.length()-1 && say[i]==say[i+1]){
-            i++;
-            count++;
-          }
-          result += to_string(count) + string( 1 , say[i]); 
-       }
-       return result;
-
-    }
+        if( n == 1){
+            return "1";
+        }
+        string result  = "";
+        string say = countAndSay(n-1);
+        for( int i = 0  ; i< say.length() ; i++){
+            char ch = say[i];
+            int count = 1;
+           while( i< say.length()-1 && say[i] == say[i+1]){
+                 count++;
+                 i++;
+           }
+           result += to_string(count) + string( 1 , ch);
+        }
+        return result;
+    } 
 };
