dfc12417c13cd58689dad7c7d28936e3dd44e2c4
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-09-30 19:02:01 +0530

    Time: 0 ms (100%), Space: 18.5 MB (28.93%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -6,26 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool check(TreeNode* a, TreeNode* b) {
-        if (a == NULL && b == NULL)
-            return true;
-        if (a == NULL || b == NULL)
-            return false;
-        return (a->val == b->val) && check(a->left, b->right) &&
-               check(a->right, b->left); //condition yad rkhni h yeh ki a->left or b->right then a->right b->left
-    }
+     bool ischeck(TreeNode*left , TreeNode*right){
+        if(!left && !right)return true;
+        if(!left || !right) return false;
+        if(left->val != right->val) return false;
+        return ischeck(left->left , right->right) && ischeck(left->right , right->left);
+     }
     bool isSymmetric(TreeNode* root) {
-        // If the tree is empty, it is symmetric
-        if (root == NULL) {
+        if(root == NULL){
             return true;
         }
-        // Check if the left and right subtrees are mirror images
-        return check(root->left, root->right);
+        return ischeck(root->left, root->right);
+        
     }
 };
