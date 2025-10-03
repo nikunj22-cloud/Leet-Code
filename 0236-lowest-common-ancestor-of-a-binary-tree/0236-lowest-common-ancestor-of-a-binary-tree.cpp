56cd0a7c295aaf06cc6371ad9c4c97290562438e
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-03 10:34:44 +0530

    Time: 12 ms (63%), Space: 17.3 MB (88.46%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,18 +10,35 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-      if(root == NULL)return NULL;
-      if(root==p || root==q)return root; //remember node ki value unique h root ki same ho skti h
+        if(root == NULL){
+            return NULL;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left , p , q);
+        TreeNode*right = lowestCommonAncestor(root->right , p , q);
 
-      TreeNode*leftN = lowestCommonAncestor(root->left , p ,q);
-      TreeNode*rightN = lowestCommonAncestor(root->right , p ,q);
+        if( root == p || root == q){
+            return root;
+        }
+        if(left != NULL && right != NULL){
+            return root;
+        }
+        //Iska Matlab
 
-      if(leftN != NULL && rightN != NULL){
-        return root;
-      }      
-      if(leftN != NULL) // but rightis
-      return leftN;
+// Humne lowestCommonAncestor(root->left, p, q) call karke left subtree me search kiya. Agar p ya q left subtree me mil gaya, toh left != NULL hoga.
 
-      return rightN; //but left is
+// Waise hi lowestCommonAncestor(root->right, p, q) se right subtree check kiya. Agar p ya q right subtree me hai, toh right != NULL hoga.
+
+// \U0001f449 Ab agar ek node left me mila aur dusra right me → iska matlab kya hua?
+
+// Dono current root ke alag-alag subtrees me mile.
+
+// Toh root hi unka common ancestor hua.
+
+// Aur kyunki ye condition tabhi true hogi jab dono side se mila hai → ye hi Lowest Common Ancestor bhi hai.
+//unka common lowest root hee hoga kyoki dono subtree m se mile h 
+        if(left != NULL){
+            return left;
+        }
+        return right;
     }
 };
