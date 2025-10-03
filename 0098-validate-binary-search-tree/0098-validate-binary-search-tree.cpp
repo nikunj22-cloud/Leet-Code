04ddcfa06c6912e189c4195fe5649e84ab484ca0
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-03 10:26:45 +0530

    Time: 0 ms (100%), Space: 21.9 MB (46.44%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,14 +11,20 @@
  */
 class Solution {
 public:
-    bool checkBST( TreeNode *root , long min , long max ){
-        if(root == NULL)return true;
-        if(root->val >= max || root->val <= min)return false;
-
-        return checkBST( root->left , min , root->val) && checkBST( root->right , root->val , max);
-
-    }
+      bool solve(TreeNode* root , long minimum , long maximum){
+         if(!root) return true;
+          if(root->val <= minimum || root->val >= maximum){
+            return false;
+          }
+        bool leftsubtree = solve(root->left , minimum , root->val);
+        bool  rightsubtree = solve(root->right , root->val , maximum);
+         return (leftsubtree && rightsubtree) ;
+      }
     bool isValidBST(TreeNode* root) {
-        return checkBST( root , LONG_MIN ,LONG_MAX);   
+      return solve(root ,LONG_MIN , LONG_MAX);
+     
     }
 };
+//iss qus m yeh method se solve krna normal h kyoki qus m saaf likha h The left subtree of a node contains only nodes with keys strictly less than the node's key.
+// The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+//Both the left and right subtrees must also be binary search trees. toh phle hmko left subtree ko right subtree ko ek ek krke check krna pdega ki sab true h or false then aage ka pta chlega 
