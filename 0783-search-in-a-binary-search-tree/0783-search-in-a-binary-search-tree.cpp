4c01e35c139a6333bd55bd8feb499090195582b1
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-03 09:52:10 +0530

    Time: 0 ms (100%), Space: 35.4 MB (61.3%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -12,12 +12,12 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root== NULL || root->val == val)return root;
-        if(root->val > val){
-            return  searchBST( root->left , val);
-        }
-        else{
-           return  searchBST( root -> right , val);
-        }
+         if(root==NULL)return root;
+         if(root->val== val)return root;
+
+         if(root->val < val)
+         return searchBST(root->right , val);
+         else
+         return searchBST(root->left , val);
     }
-};
+}; //sub tree roted return krni h toh mtlb recursion hee chlega return m or binary serch tree h toh mtlb value k basic pr hee ans find hoga
