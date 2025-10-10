9e92705c5b0e6d9e88f51093f7651861ebbf2f9a
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-10 11:23:00 +0530

    Time: 0 ms (100%), Space: 15.8 MB (33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-         int n = nums.size();
-          int gola_index = -1;
-         for( int i = n-1 ; i> 0 ; i--){ //remeber i > 0 
-            if( nums[i-1] < nums[i]){
-                gola_index = i-1;
-                break;
-            }
-         }
-         int  swap_index = gola_index;
-         if(  gola_index != -1){
-         for( int j = n-1 ; j>= gola_index+1 ; j--){ //remember gola_index+1
-               if( nums[j] > nums[gola_index]){
-                swap_index = j;
+       int n = nums.size();
+       int gola_index = -1;
+       for( int i = n-1 ; i> 0 ; i--){ // remember i > 0
+        if( nums[i-1] < nums[i]){
+             gola_index = i-1;
+            break;
+        }
+       } 
+        int swapindex = gola_index;
+        if( gola_index != -1){
+            for( int j = n-1 ; j>= gola_index+1 ; j--){
+               if( nums[gola_index]  < nums[j]){
+                swapindex = j;
                 break;
                }
-         }
-               swap( nums[gola_index] , nums[swap_index]);
-              
-         }
-      reverse( nums.begin()+gola_index+1 , nums.end());
+            }
+            swap( nums[gola_index] , nums[swapindex]);
+        }
+        reverse( nums.begin()+ gola_index+1 , nums.end() );
     }
 };
