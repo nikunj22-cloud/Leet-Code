1d97388320d48605d10bb39d38cc67e9165ed03a
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-10 08:31:10 +0530

    Time: 4 ms (13.5%), Space: 17.8 MB (22.81%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        int result = nums[0];
         int maxpro = nums[0];
-        int pro = 1;
-        for( int i = 0 ; i<nums.size() ; i++){
-            pro *=  nums[i];
-            maxpro = max( maxpro , pro);
-
-            
-        if( pro < 0)
-          pro = 1;
+        int minpro = nums[0];
+    
+        for( int i = 1 ; i<nums.size() ; i++){
+            int curr = nums[i];
+             
+            int tempmax = max({curr*minpro , maxpro*curr , curr});
+             int tempmin = min( {maxpro*curr , minpro*curr , curr});
+              maxpro = tempmax;
+              minpro = tempmin; //current bar bar maxpro ko change krrha h isliye uper k multiply se maxpro alg ho jayega isliye neeche varaible liye h
+            result = max( maxpro , result);
         }
-
-      return maxpro ;
+      return result;  
     }
 };
