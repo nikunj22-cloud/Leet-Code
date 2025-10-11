2c5883e6e2802c96075c830a87e3249fd5b0719e
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-12 01:18:31 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.18%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    double overflowsebachna( double x , long n){ //long overflow se bachne k liye 2ki power -31 se +31 tk
-        if( n == 0){
-            return 1;
-        }
-        if( n < 0){
-           return  overflowsebachna( 1/x , -n);
-        }
-        if( n%2 == 0)
-            return  overflowsebachna( x*x , n/2);
-            return   x*overflowsebachna(x*x , (n-1)/2);
-        
-    }
+   double power( double x , long n){
+       if( n == 0){
+        return 1;
+       }
+       if( n <  0){
+        return power( 1/x , -n);
+       }
+       if( n % 2 == 0)return power( x*x , n/2);
+       return x * power( x*x , (n-1)/2);
+   }
     double myPow(double x, int n) {
-        return overflowsebachna( x , (long)n);
+         return power( x, (long) n);
     }
 };
