6aee22526f0a3beb835ae6b95127ade33be133c9
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-11 17:37:33 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.47%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size(); int i , j;
-        //transpose
-        for( int i =0 ; i< n ; i++){
-            for( int j = i ; j< n ; j++){
-                 swap( matrix[i][j], matrix[j][i]);
-            }
+       int m = matrix.size();
+       int n = matrix[0].size();
+
+       for( int i = 0 ; i<m ; i++){
+        for(int j = i ; j< n; j++){
+            swap( matrix[i][j] , matrix[j][i]);
         }
-        //reverse
-        for( int i =  0 ; i<n ; i++){
-            reverse(matrix[i].begin() ,matrix[i].end());
-        
-        }
+       }
+       for( int i = 0 ; i< m ; i++){
+        reverse( matrix[i].begin() , matrix[i].end());
+       }
     }
 };
+//\U0001f539 Goal: Transpose banana
+
+// Transpose ka matlab hota hai —
+// \U0001f449 Rows ko Columns me badal dena.
+// Ya fir, matrix[i][j] aur matrix[j][i] ko swap kar dena.
+
+// Lekin agar har element ko dono taraf se swap kar doge (double swap),
+// to original hi reh jayega \U0001f605
+// isiliye hum j = i se start karte hain.
+
+//Kyuki:
+
+// reverse() function poori row ko khud hi reverse kar deta hai
+
+// Hume manually j loop lagake swap() karne ki zarurat nahi
+
+// i bas row number batata hai ki ab kis row ko reverse karna hai
