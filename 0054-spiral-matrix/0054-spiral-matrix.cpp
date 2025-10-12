a3965df728eb68f0de035c7d61ecd43b64e5335a
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-13 00:06:10 +0530

    Time: 0 ms (100%), Space: 9.5 MB (13.69%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -3,36 +3,36 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        int top = 0;
-        int down = m-1;
         int left = 0;
         int right = n-1;
-        vector<int>result;
-        while( top <= down && left <= right){
-        for( int i = left ; i<= right ; i++){
-              result.push_back(matrix[top][i]);
-        }
-              top++; // 3 to 6
-        
-        for( int i = top ; i <= down ; i++){
-            result.push_back(matrix[i][right]);
-        }
-            right--;//9 to 8
-         
-            if (top <= down) {
-        for( int i = right ; i >= left ; i--){
-            result.push_back(matrix[down][i]);
-        }
-            down--;//7 to 4
+        int top = 0;
+        int down = m-1;
+       vector<int>result;
+        while( left <= right && top <= down){
+            for( int i = left ; i<= right ; i++){
+                result.push_back(matrix[top][i]);
             }
-        
-         if (right >= left) {
-        for( int i = down ; i >= top ; i-- ){
-            result.push_back(matrix[i][left]);
+                top++;
+            
+            for( int i = top ; i<= down ; i++){
+                result.push_back( matrix[i][right]);
+            }
+            right--;
+
+            if( down >= top){
+             for( int i =  right ; i>= left ; i--){
+                result.push_back(matrix[down][i]);
+             }
+             down--;
+            }
+            if( left <= right){
+                for( int i = down ; i >= top ; i--){
+                    result.push_back( matrix[i][left]);
+                }
+                left++;
+            }
+
         }
-            left++; //4 to 5 in qus
-        }
-    }
-    return result;
+        return result;
     }
 };
