8810c6e7c56ff0e24178201f2dcf0f8780509ace
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-10-29 16:03:24 +0530

    Time: 34 ms (32.61%), Space: 12.9 MB (84.07%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -3,20 +3,22 @@ public:
     int t[1001][1001];
 
     int solve(string &s1, string &s2, int i, int j) {
-       if (i == s1.size() || j == s2.size())
+       if (i == 0 || j == 0)
             return 0;
 
         if (t[i][j] != -1)
             return t[i][j];
 
-        if (s1[i] == s2[j])
-            return t[i][j] = 1 + solve(s1, s2, i + 1, j + 1);
+        if (s1[i-1] == s2[j-1])
+            return t[i][j] = 1 + solve(s1, s2, i-1, j-1);
 
-        return t[i][j] = max(solve(s1, s2, i + 1, j), solve(s1, s2, i, j + 1));
+        return t[i][j] = max(solve(s1, s2, i - 1, j), solve(s1, s2, i, j - 1));
     }
 
     int longestCommonSubsequence(string text1, string text2) {
         memset(t, -1, sizeof(t));
-        return solve(text1, text2, 0, 0);
+        int m = text1.length();
+        int n = text2.length();
+        return solve(text1, text2, m, n);
     }
 };
