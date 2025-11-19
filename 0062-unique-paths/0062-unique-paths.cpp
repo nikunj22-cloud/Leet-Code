8ecd322a7c0a92c2a214a3ec295e2c6b0dfdf7a5
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-11-19 22:48:08 +0530

    Time: 0 ms (100%), Space: 8 MB (87.17%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,22 @@
 class Solution {
-public: 
-      int t[101][101];
-       int path( int m  , int n , int i , int j){
-        if( i < 0 || i > m || j < 0 || j > n  ){
-            return 0 ;
+public:  
+     int t[101][101];
+     int path( int m , int n , int i , int j){
+        if( i < 0 || i > m || j < 0 || j > n ){
+            return 0;
         }
-        if( i == m-1 && j == n-1 ){
+         if( t[i][j] != -1){
+            return t[i][j];
+         }
+        if( i == m-1 && j == n-1){
             return 1;
         }
-        if( t[i][j] != -1){
-            return t[i][j];
-        }
-         int down = path( m ,n , i+1 , j);
+         int down = path( m , n ,i+1 , j );
          int right = path( m , n , i , j+1);
-         return  t[i][j] = down+right ;
-       }
+         return  t[i][j] = down+right;
+     }
     int uniquePaths(int m, int n) {
          memset( t , -1 , sizeof(t));
-         return path(m , n  , 0 ,0);
+         return path( m , n , 0 ,0);
     }
 };
