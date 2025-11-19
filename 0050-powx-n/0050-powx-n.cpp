67702ea3753cd663053532b29a639507e24dc323
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-11-19 19:52:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.84%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,18 @@
 class Solution {
-public:
-   double power( double x , long n){
-       if( n == 0){
+public: 
+   double pow( double x , long n){
+    if( n == 0){
         return 1;
-       }
-       if( n <  0){
-        return power( 1/x , -n);
-       }
-       if( n % 2 == 0)return power( x*x , n/2);
-       return x * power( x*x , (n-1)/2);
+    }
+    if( n < 0){
+        return pow( 1/x , -n);
+    }
+    if( n%2 == 0 ){
+        return pow( x*x , n/2);
+    }
+    return  x*pow(x*x , (n-1)/2);
    }
     double myPow(double x, int n) {
-         return power( x, (long) n);
+        return pow( x , long(n));
     }
 };
