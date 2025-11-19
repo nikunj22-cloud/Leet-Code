ba7332fdadf3c240ba1d1194b42f749e51e16331
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-11-19 20:04:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count =0;
-        int majority = 0 ;
-        for( int i = 0 ; i<nums.size() ; i++){
-           if(count==0) //sabse phle count 0 h toh majority assign krdo because You may assume that the majority element always exists in the array.
-            majority = nums[i];
+        int maj = 0;
+        int count = 0;
+        for( int i = 0 ; i< nums.size() ;  i++){
+            if( count == 0){
+                maj = nums[i];
+            }
+           if(nums[i] = maj){
             count++;
-        
-           if(nums[i]== majority){
-            count++; //ab agr traverse krte krte ek or nums[i] mila joh majority k jitna h toh count ++ krdo
            }
            else{
-            count--; //nhi toh count--  mtlb woh element ek bar hee aya h toh kya fayda usko majority manne ka agr ek bar aaya hoga toh zero hojayega - hoker then count ek new majority assign krlega
+            count--;
            }
         }
-        return majority;
+        return maj;
     }
 };
