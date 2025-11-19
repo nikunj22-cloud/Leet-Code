c91d57a9580d55d58fa044df66caddd61fbd43de
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-11-19 09:50:05 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.12%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>ans;
-        sort(intervals.begin() , intervals.end());
-        for( int i = 0 ; i<intervals.size() ; i++){
+         vector<vector<int>>ans;
+         sort( intervals.begin() , intervals.end());
+         for( int i = 0 ; i< intervals.size(); i++){
             if( ans.empty() || intervals[i][0] > ans.back()[1]){
-                ans.push_back(intervals[i]);
+                ans.push_back( intervals[i]);
             }
             else{
-                ans.back()[1] = max( ans.back()[1] , intervals[i][1]);
+                ans.back()[1] = max( ans.back()[1] , intervals[i][1]); 
             }
-        }
-        return ans;
+         }
+         return ans;
     }
 };
