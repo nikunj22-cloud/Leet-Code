e3ee66eae3aec416a00df31a70af72112cf9cb14
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-11-19 10:32:09 +0530

    Time: 0 ms (100%), Space: 9.4 MB (49.45%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-         int m = matrix.size();
-         int n = matrix[0].size();
-         int left = 0;
-         int right = n-1;
-         int top = 0;
-         int down = m-1;
-         vector<int>result;
-         while( left <= right && top <= down){
-              for( int i = left ; i<= right ; i++){
+          vector<int>result;
+          int m = matrix.size();
+           int n = matrix[0].size();
+           int top = 0;
+           int down = m-1;
+           int left = 0;
+           int right = n-1;
+           while( left <= right && top <= down){
+            for( int i = left ; i<= right ; i++){
                 result.push_back( matrix[top][i]);
-              }
-              top++; // 3-6
-              for( int j = top ; j<= down ; j++){
-                result.push_back( matrix[j][right]);
-              }
-              right--; // 9-8
-
-              if( down >= top){
-              for( int i = right ; i >= left ; i-- ){
+            }
+            top++;
+            for( int i = top ; i<= down ; i++){
+                result.push_back( matrix[i][right]);
+            }
+            right--;
+            if( down >= top){
+            for( int i = right ; i>= left ; i--){
                 result.push_back( matrix[down][i]);
-              }
-              down--;
-              }
-              if( right>= left){
-              for( int j = down ; j >= top ; j--){
-                result.push_back( matrix[j][left]);
-              }
-              left++;
+            }
+            down--;
            }
-         }
-         return result;
+           if( right >= left){
+            for( int i = down ; i >= top ; i--){
+                result.push_back( matrix[i][left]);
+            }
+            left++;
+           }
+           }
+           return result;
     }
 };
+// //Agar code me top-- kare → pointer upar chale jayega
+// Par spiral me kabhi upar nahi jaate, andar ki taraf jaate ho.
+//❌ down++ = neeche
+// Par spiral me iski zarurat hi nahi hoti.
