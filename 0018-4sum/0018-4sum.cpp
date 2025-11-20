d5464f720493fedf23fa0b46519492c88991b26e
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-11-20 18:28:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-      vector<vector<int>>result;
-      sort(nums.begin() , nums.end());
-      int n = nums.size();
-      for( int i = 0 ; i< n ; i++){
-
-        for( int j = i+1 ; j<n; j++){
-                    if( i > 0 && nums[i] == nums[i-1])continue;
-            if( j >i+1 && nums[j] == nums[j-1])continue;
-            int k = j+1;
-            int l = n-1;
-           
-            while( k < l){
-                 long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
-                 //remember sum loop k ander define kiya jayega
-                if( sum == target ){
-                    result.push_back({nums[i] , nums[j] , nums[k] , nums[l]});
+         vector<vector<int>>result;
+         sort( nums.begin() , nums.end());
+         int n = nums.size();
+         for( int i = 0 ; i< n; i++){
+             for( int j = i+1 ; j<n; j++){
+                if(  i > 0 && nums[i] == nums[i-1])continue;
+                if(  j > i+1 && nums[j] == nums[j-1])continue;
+             
+             int k = j+1;
+             int l = n-1;
+             while( k < l){
+                long long  sum = nums[i] + nums[j] + nums[k] + nums[l];
+                if( sum == target){
+                    result.push_back({ nums[i] , nums[j] , nums[k] , nums[l]});
                     k++;
                     l--;
-                    while( k < l && nums[k] == nums[k-1] )k++;
-                    while( k < l && nums[l] == nums[l+1])l--;
+                
+                while( k < l && nums[k] == nums[k-1]) k++;
+                while( k < l && nums[l] == nums[l+1]) l--;
                 }
-                else if( sum < target){
-                    k++;
-                }
-                else {
+                  else if( sum > target){
                     l--;
-                }
-            }
-        } 
-      }
-      return result;   
+                 }
+                 else{
+                    k++;
+                 }
+             }
+         }
+         }
+         return result;
     }
 };
