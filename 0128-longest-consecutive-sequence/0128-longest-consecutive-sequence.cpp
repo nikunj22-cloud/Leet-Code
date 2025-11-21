7557f87bfb976095fa90dd91679c68039ec387b2
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-11-21 20:09:48 +0530

    Time: 95 ms (24.71%), Space: 89 MB (21.49%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -24,3 +24,32 @@ public:
     return ans;
     }
 };
+//bhai yad rkhna or kisi m ni hoga ans m next-prev-1 hoga 
+//Example se clear karte hain
+
+// Suppose sequence hai:
+
+// [1, 2, 3, 4, 5]
+
+
+// maan lo current nums[i] = 3
+
+// After while loops:
+// prev = 0   (kyunki 1 ke baad 0 nahi mila)
+// next = 6   (kyunki 5 ke baad 6 nahi mila)
+
+
+// Ab actual consecutive numbers hain:
+
+// 1, 2, 3, 4, 5
+
+
+// Length = 5
+
+// Formula:
+
+// next - prev - 1
+// = 6 - 0 - 1
+// = 5 ✅
+
+// General Math Logic
